c2cb8e0d2786330191cce086c3df52ad87a44254
RameshRaju <RameshRaju@alluri>
2015-11-14 11:28:00 +0530

    added nav actions

--- public_html/js/controls.js
+++ public_html/js/controls.js
@@ -9,6 +9,18 @@ app.controller('homeCtrl', ['$scope','$http', function ($scope, $http) {
      $scope.lstForms=[{id:1,frmDB:'Dash Board',slted:'active',Show:true},
       {id:2,frmDB:'Master Creation',slted:'',Show:true},
       {id:3,frmDB:'Client Master',slted:'',Show:true},
-        {id:4,frmDB:'Reports',slted:'',Show:true}];
-//    $scope.ngtest="ramesh";
+      {id:4,frmDB:'Reports',slted:'',Show:true}];
+$scope.divSelect=1;
+        $scope.divShow=function(vId){
+            $scope.divSelect=vId;
+            angular.forEach($scope.lstForms,function(l){
+               if(angular.equals(vId,l.id)){
+                   l.slted='active';
+               }else{
+                   l.slted='';
+               } 
+               
+            });
+        };
+
   }]);
